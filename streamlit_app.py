59bb8c1fcb43112d670df0f843e60ebcbf1cbcd9
rahul-rawat2 <132780039+rahul-rawat2@users.noreply.github.com>
2023-05-07 00:33:13 +0530

    Update streamlit_app.py
    Breakfast

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,11 @@
 import streamlit
 
 streamlit.title('My Parents New Healthy Diner')
+
 Breakfast Menu
+
 Omega 3 & Blueberry Oatmeal
-Kale spinach & Rocket Smoothie
+
+Kale, Spinach & Rocket Smoothie
+
 Hard-Boiled Free-Range Egg
