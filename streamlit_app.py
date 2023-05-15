e39b3caffa836dbd8d6de0d1a3e4dda1107b5d4d
rahul-rawat2 <132780039+rahul-rawat2@users.noreply.github.com>
2023-05-15 17:06:03 +0530

    Update streamlit_app.py
    Fruit choice

--- streamlit_app.py
+++ streamlit_app.py
@@ -28,6 +28,8 @@ fruits_to_show = my_fruit_list.loc[fruits_selected]
 streamlit.dataframe(fruits_to_show)
 
 streamlit.header("Fruityvice Fruit Advice!")
+fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
+streamlit.write('The user entered ', fruit_choice)
 
 
 import requests
@@ -41,6 +43,4 @@ fruityvice_normalized = pandas.json_normalize(fruityvice_response.json())
 streamlit.dataframe(fruityvice_normalized)
 
 
-fruit_choice = streamlit.text_input('What fruit would you like information about?','Kiwi')
-streamlit.write('The user entered ', fruit_choice)
 
